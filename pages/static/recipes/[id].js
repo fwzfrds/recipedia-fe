39574440ffbe47d928e7ae85b23c4797114c82f3
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-06-28 10:43:13 +0700

    Mencoba nonaktifkan fitur SSG untuk cek deployment di vercel

--- pages/static/recipes/[id].js
+++ pages/static/recipes/[id].js
@@ -1,57 +1,57 @@
-import React from 'react'
-import axios from 'axios';
+// import React from 'react'
+// import axios from 'axios';
 
-const StaticDetailRecipe = ({recipe}) => {
+// const StaticDetailRecipe = ({recipe}) => {
 
-    console.log(recipe)
+//     console.log(recipe)
 
-    return (
-        <div>
-            <h1>Static Recipe Detail</h1>
-            <ul>
-                <li>{recipe && recipe.name}</li>
-                <li>{recipe && recipe.email}</li>
-            </ul>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>Static Recipe Detail</h1>
+//             <ul>
+//                 <li>{recipe && recipe.name}</li>
+//                 <li>{recipe && recipe.email}</li>
+//             </ul>
+//         </div>
+//     )
+// }
 
-export async function getStaticPaths() {
-    // const id = context.params.id
-    // const { data: respData } = await axios.get(`http://localhost:4000/v1/recipes`)
-    const { data: respData } = await axios.get(`https://jsonplaceholder.typicode.com/users`)
-    // console.log(respData)
+// export async function getStaticPaths() {
+//     // const id = context.params.id
+//     // const { data: respData } = await axios.get(`http://localhost:4000/v1/recipes`)
+//     const { data: respData } = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+//     // console.log(respData)
 
-    const dataRecipes = respData
+//     const dataRecipes = respData
 
-    const paths = dataRecipes.map((recipe) => {
-        return {
-            params: {
-                id: recipe.id + ''
-            }
-        }
-    })
+//     const paths = dataRecipes.map((recipe) => {
+//         return {
+//             params: {
+//                 id: recipe.id + ''
+//             }
+//         }
+//     })
 
-    console.log(paths)
+//     console.log(paths)
 
-    return {
-        paths: paths,
-        fallback: true // false or 'blocking'
-    };
-}
+//     return {
+//         paths: paths,
+//         fallback: true // false or 'blocking'
+//     };
+// }
 
-export async function getStaticProps(context) {
-    const id = context.params.id
-    // const { data: RespData } = await axios.get(`http://localhost:4000/v1/recipes/detail/${id}`)
-    const { data: RespData } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+// export async function getStaticProps(context) {
+//     const id = context.params.id
+//     // const { data: RespData } = await axios.get(`http://localhost:4000/v1/recipes/detail/${id}`)
+//     const { data: RespData } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
 
-    console.log(RespData) // test test
+//     console.log(RespData) // test test
 
-    return {
-        props: {
-            recipe: RespData
-        }
-    }
-}
+//     return {
+//         props: {
+//             recipe: RespData
+//         }
+//     }
+// }
 
-export default StaticDetailRecipe
+// export default StaticDetailRecipe
